845f26604d8d95d0fed18bac2965975072ad1acd
Hannes Walz <hannes@leuchtetgruen.de>
2015-05-24 21:15:20 +0200

    double decrypt error

--- crypto.rb
+++ crypto.rb
@@ -9,7 +9,6 @@ end
 
 def decrypt_file(in_file, out_file, password)
 	openssl_command = "openssl aes-256-cbc -d -in \"#{in_file}\" -out \"#{out_file}\" -pass pass:#{password}"
-	system(openssl_command)
 	if !system(openssl_command) then
 		puts "FATAL: Error while decrypting file"
 		exit
